dc73e732af6d0708b3b60a7edbaffa42f3c35858
pratiktamang <pratiktamang13@gmail.com>
2022-03-17 00:07:47 +0000

    Sidebar Componenet

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -37,6 +37,21 @@ const navMenu = [
 	},
 ]
 
+const musicMenu = [
+	{
+		name: 'Create Playlist',
+		icon: MdLibraryAdd,
+		route: '/'
+	},
+	{
+		name: 'Favorites',
+		icon: MdFavorite,
+		route: '/favorites'
+	}
+]
+
+const playlists = new Array(30).fill(0).map((_, i) => `Playlist ${i + 1}`)
+
 const Sidebar = () => {
   return (
 		<Box 
@@ -46,7 +61,7 @@ const Sidebar = () => {
 		paddingX="5px" 
 		color="gray"
 		>
-			<Box paddingY="20px">
+			<Box paddingY="20px" height="100%">
 				<Box width="120px" marginBottom="20px" paddingX="20px">
 					<NextImage src="/logo.svg" height={60} width={120}/>
 				</Box>
@@ -63,6 +78,42 @@ const Sidebar = () => {
 												marginRight="20px"
 											/>
 											{menu.name}
+										</LinkOverlay>
+									</NextLink>
+								</LinkBox>
+							</ListItem>
+						))}
+					</List>
+				</Box>
+				<Box marginY="20px">
+					<List spacing={2}>
+						{musicMenu.map(menu => (
+							<ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+								<LinkBox>
+									<NextLink href={menu.route} passHref>
+										<LinkOverlay>
+											<ListIcon
+												as={menu.icon}
+												color="white"	
+												marginRight="20px"
+											/>	
+											{menu.name}
+										</LinkOverlay>
+									</NextLink>
+								</LinkBox>
+							</ListItem>
+						))}
+					</List>
+				</Box>
+				<Divider color="gray.800" />
+				<Box height="66%" overflowY="auto" paddingY="20px">
+					<List spacing={2}>
+						{playlists.map(playlist => (
+							<ListItem paddingX="20px" fontSize="16px" key={playlist}>
+								<LinkBox>
+									<NextLink href={`/playlist/${playlist}`} passHref>
+										<LinkOverlay>
+											{playlist}
 										</LinkOverlay>
 									</NextLink>
 								</LinkBox>
